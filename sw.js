76db971c063972cab2b29a0d8c0f6514f92bdc4b
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-09-03 10:57:08 -0600

    Update Cache Service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,26 @@
 const CACHE_ELEMENTS = [
     "./",
-    "https://kit.fontawesome.com/a6ee538727.js",
-    
-  
+
+    "/assets/styles/minified.css",
+
+    "./assets/images/ico.png",
+    "./assets/images/ico-xl.png",
+    "./assets/images/knowledge/alura.webp",
+    "./assets/images/knowledge/certiprof.webp",
+    "./assets/images/knowledge/cs50.webp",
+    "./assets/images/knowledge/ucr.webp",
+    "./assets/images/projects/flags.webp",
+    "./assets/images/projects/password.webp",
+    "./assets/images/projects/planets.webp",
+    "./assets/images/projects/routes.webp",
+    "./assets/images/projects/rps.webp",
+    "./assets/images/projects/shortly.webp",
+    "./assets/images/projects/todo.webp",
+    "./assets/images/projects/tracker.webp",
+
+    "https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css",
+    "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css",
 ];
 
 const CACHE_NAME = "v1_Portfolio-APP";
@@ -30,6 +48,6 @@ self.addEventListener("activate", (e) =>
 
 self.addEventListener("fetch", (e) => {
     e.respondWith(caches.match(e.request)
-        .then(res => res ? res : fetch(e.request) )
+        .then(res => res ? res : fetch(e.request))
     );
 });
