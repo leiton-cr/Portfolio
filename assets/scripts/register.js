d2d350752453a168d40d44ae802268ceba0bebf7
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-09-02 23:22:59 -0600

    Update Service worker y Cache

--- assets/scripts/register.js
+++ assets/scripts/register.js
@@ -1,3 +1,3 @@
 if ("serviceWorker" in navigator){
-    navigator.serviceWorker.register('./assets/scripts/sw.js')
+    navigator.serviceWorker.register('sw.js')
 }
---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_ELEMENTS = [
+    "./",
+  
+];
+
+const CACHE_NAME = "v1_Portfolio-APP";
+
+const CACHE_WHITE_LIST = [CACHE_NAME];
+
+
+self.addEventListener("install", e =>
+    e.waitUntil(caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(CACHE_ELEMENTS))
+        .then(() => self.skipWaiting())
+        .catch(console.log)
+    )
+);
+
+
+self.addEventListener("activate", (e) =>
+    e.waitUntil(caches.keys()
+        .then(cacheNames =>
+            cacheNames.map(cache => !CACHE_WHITE_LIST.includes(cache) && caches.delete(cache)))
+        .then(() => self.clients.claim())
+        .catch(console.log)
+    )
+);
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(caches.match(e.request)
+        .then(res => res ? res : fetch(e.request) )
+    );
+});
